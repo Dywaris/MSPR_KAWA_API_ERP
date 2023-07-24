e1c38763cbed2015ae2f9d90686009080cb8846c
Dywaris1 <julien.dezerable@epsi.fr>
2023-07-24 14:40:47 +0200

    add get product

--- routes/products.js
+++ routes/products.js
@@ -17,6 +17,20 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    https.get('https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products/' + req.params.id, function(rep) {
+        var rawData = '';
+        rep.on('data', (chunk) => { rawData += chunk; console.log('BUFFER',rawData); });
+        rep.on('end', () => {
+            try {
+                res.send(rawData);
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
+    });
+});
+
 
 module.exports = router;
 
