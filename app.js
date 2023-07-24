daf84a74be8cbab948ba2737d705804de9eca897
Dywaris1 <julien.dezerable@epsi.fr>
2023-07-24 14:30:26 +0200

    init express

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const port = "3000";
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
